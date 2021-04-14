c8a0a865aa480af7cfd6140f5e4df7d2b5bec3f6
alfred richardsn <rchrdsn@protonmail.ch>
2021-04-14 05:05:37 +0300

    Create storage for dispatcher

--- cradlex/bot.py
+++ cradlex/bot.py
@@ -2,7 +2,14 @@ from aiogram import Bot
 from aiogram import Dispatcher
 
 from cradlex import config
+from cradlex import database
+from cradlex.i18n import i18n
+from cradlex.user import user_middleware
 
 with open(config.TOKEN_FILENAME, "r") as token_file:
     bot = Bot(token_file.read().strip())
+
 dp = Dispatcher(bot)
+dp.storage = database.PostgreStorage()
+dp.middleware.setup(user_middleware)
+dp.middleware.setup(i18n)
--- cradlex/database.py
+++ cradlex/database.py
@@ -1,19 +1,99 @@
+import json
+from string import Template
+
 import sqlalchemy as sa
 import sqlalchemy.ext.asyncio
+from aiogram.dispatcher.storage import BaseStorage
 
 from cradlex import config
+from cradlex import models
 
 
-with open(config.DATABASE_PASSWORD_FILENAME, "r") as password_file:
-    engine = sa.ext.asyncio.create_async_engine(
-        "postgresql+asyncpg://{user}:{password}@{host}:{port}/{name}".format(
-            host=config.DATABASE_HOST,
-            port=config.DATABASE_PORT,
-            user=config.DATABASE_USERNAME,
-            password=password_file.read().strip(),
-            name=config.DATABASE_NAME,
-        )
+URL_TEMPLATE = Template(
+    Template("postgresql+asyncpg://$auth@$host:$port/$name").safe_substitute(
+        host=config.DATABASE_HOST,
+        port=config.DATABASE_PORT,
+        name=config.DATABASE_NAME,
     )
+)
+try:
+    with open(config.DATABASE_PASSWORD_FILENAME, "r") as password_file:
+        URL = URL_TEMPLATE.substitute(
+            auth=f"{config.DATABASE_USERNAME}:{password_file.read().strip()}"
+        )
+except (AttributeError, FileNotFoundError):
+    URL = URL_TEMPLATE.substitute(auth=f"{config.DATABASE_USERNAME}")
 
 
-sessionmaker = sa.orm.sessionmaker(engine, class_=sa.ext.asyncio.AsyncSession)
+engine = sa.ext.asyncio.create_async_engine(URL)
+sessionmaker = sa.orm.sessionmaker(
+    engine,
+    expire_on_commit=False,
+    class_=sa.ext.asyncio.AsyncSession,
+)
+
+
+class PostgreStorage(BaseStorage):
+    async def close(self):
+        pass
+
+    async def wait_closed(self):
+        pass
+
+    async def get_state(self, *, chat=None, user, **kwargs):
+        async with sessionmaker() as session:
+            async with session.begin():
+                cursor = await session.execute(
+                    sa.select(models.User.state).where(models.User.id == user)
+                )
+                return cursor.scalar()
+
+    async def get_data(self, *, chat=None, user, **kwargs):
+        async with sessionmaker() as session:
+            async with session.begin():
+                cursor = await session.execute(
+                    sa.select(models.User.data).where(models.User.id == user)
+                )
+                return cursor.scalar()
+
+    async def set_state(self, *, chat=None, user, state=None):
+        async with sessionmaker() as session:
+            async with session.begin():
+                await session.execute(
+                    sa.update(models.User)
+                    .values(state=state)
+                    .where(models.User.id == user)
+                )
+
+    async def set_data(self, *, chat=None, user, data):
+        async with sessionmaker() as session:
+            async with session.begin():
+                await session.execute(
+                    sa.update(models.User)
+                    .values(data=json.dumps(data))
+                    .where(models.User.id == user)
+                )
+
+    async def update_data(self, *, chat=None, user, data=None, **kwargs):
+        if data is None:
+            data = {}
+        data.update(kwargs)
+        async with sessionmaker() as session:
+            async with session.begin():
+                await session.execute(
+                    sa.update(models.User)
+                    .values(data=sa.text(f"data || '{json.dumps(data)}'"))
+                    .where(models.User.id == user)
+                )
+
+    async def reset_state(self, *, chat=None, user, with_data=True):
+        values = {"state": None}
+        if with_data:
+            values["data"] = sa.text("'{}'")
+        async with sessionmaker() as session:
+            async with session.begin():
+                await session.execute(
+                    sa.update(models.User)
+                    .values(**values)
+                    .where(models.User.id == user)
+                )

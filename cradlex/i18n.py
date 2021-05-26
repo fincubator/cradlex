d107acdabdc58400d89235f9bdbf0683c01588f7
alfred richardsn <rchrdsn@protonmail.ch>
2021-05-26 18:09:32 +0300

    Use default locale in ctx_locale

--- cradlex/i18n.py
+++ cradlex/i18n.py
@@ -1,9 +1,14 @@
+from contextvars import ContextVar
 from pathlib import Path
 
 from aiogram.contrib.middlewares.i18n import I18nMiddleware
 
 
 class I18nMiddlewareProxy(I18nMiddleware):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.ctx_locale = ContextVar(self.ctx_locale.name, default=self.default)
+
     async def get_user_locale(self, *args, **kwargs) -> str:
         # Fixes https://github.com/aiogram/aiogram/issues/562
         language = await super().get_user_locale(*args, **kwargs)
